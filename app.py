5113d9add1f8f98e30c6f064cb277d4b3edb069d
SparshJain2000 <jainsparsh0801@gmail.com>
2021-05-31 01:00:40 +0530

    post image

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, send_from_directory, request
 from flask_restful import Api, Resource
+from datetime import datetime
+from werkzeug.utils import secure_filename
 
+import os
 app = Flask(__name__)
 api = Api(app)
 
@@ -25,6 +28,18 @@ class Images(Resource):
     def get(self, id):
         return {"image": images[id]}
 
+    def post(self):
+        likes = request.form.get('likes')
+        image = request.files.get('image')
+        path = f'{str(datetime.timestamp(datetime.now())).split(".")[0]}_{image.filename}'
+        image.save(os.path.join('data/images/',
+                                secure_filename(path)))
+        images[len(images)+1] = {
+            'likes': int(likes),
+            'image': f'/data/images/{path}'
+        }
+        return {"images": images}
+
 
 class File(Resource):
     def get(self, path):
@@ -32,7 +47,7 @@ class File(Resource):
 
 
 api.add_resource(HelloWorld, "/")
-api.add_resource(Images, "/api/image/<string:id>")
+api.add_resource(Images, "/api/image/")
 api.add_resource(File, "/data/<path:path>")
 if __name__ == '__main__':
     app.run(debug=True)
