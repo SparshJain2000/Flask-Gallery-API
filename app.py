ecf85bb0ba09a8a4f2e6bb611b7bf2ca8e0011a2
SparshJain2000 <jainsparsh0801@gmail.com>
2021-05-31 00:24:08 +0530

    add image route

--- app.py
+++ app.py
@@ -1,13 +1,33 @@
-from flask import Flask
-from flask_restful import Api,Resource
+from flask import Flask, send_from_directory, request
+from flask_restful import Api, Resource
 
-app=Flask(__name__)
-api=Api(app)
+app = Flask(__name__)
+api = Api(app)
+
+images = {
+    "1": {
+        "image": "./data/images/sunset.jpg",
+        "likes": 0,
+    },
+    "2": {
+        "image": "./data/images/scene.jpg",
+        "likes": 0,
+    }
+}
+
 
 class HelloWorld(Resource):
     def get(self):
-        return { "message" : "Hello World" }
+        return {"message": "Hello World"}
 
-api.add_resource(HelloWorld,"/")
-if __name__=='__main__':
+
+class Images(Resource):
+    def get(self, id):
+        return {"image": images[id]}
+
+
+api.add_resource(HelloWorld, "/")
+api.add_resource(Images, "/api/image/<string:id>")
+
+if __name__ == '__main__':
     app.run(debug=True)
