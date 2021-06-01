49f5b06f222a36349e681dfa17946d63748c7aa4
SparshJain2000 <jainsparsh0801@gmail.com>
2021-06-01 13:37:04 +0530

    add author in images

--- app.py
+++ app.py
@@ -6,15 +6,18 @@ from werkzeug.utils import secure_filename
 import os
 app = Flask(__name__)
 api = Api(app)
+app.config['DEBUG'] = True
 
 images = {
     "1": {
         "image": "./data/images/sunset.jpg",
         "likes": 0,
+        "author": "Sparsh"
     },
     "2": {
         "image": "./data/images/scene.jpg",
         "likes": 0,
+        "author": "Sparsh"
     }
 }
 
@@ -36,13 +39,15 @@ class Images(Resource):
 
     def post(self):
         likes = request.form.get('likes')
+        author = request.form.get('author')
         image = request.files.get('image')
         path = f'{str(datetime.timestamp(datetime.now())).split(".")[0]}_{image.filename}'
         image.save(os.path.join('data/images/',
                                 secure_filename(path)))
         images[len(images)+1] = {
             'likes': int(likes),
-            'image': f'/data/images/{path}'
+            'image': f'/data/images/{path}',
+            'author': author
         }
         return {"images": images}
 
@@ -55,5 +60,6 @@ class File(Resource):
 api.add_resource(HelloWorld, "/")
 api.add_resource(Images, "/api/image/")
 api.add_resource(File, "/data/<path:path>")
+
 if __name__ == '__main__':
     app.run(debug=True)
