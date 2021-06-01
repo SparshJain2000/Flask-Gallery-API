4620e3588858954e5e5c6fe92604e3daa29cf4bd
SparshJain2000 <jainsparsh0801@gmail.com>
2021-06-01 22:06:55 +0530

    exception handling + env vars

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
+import os
 from flask import Flask, send_from_directory, request, json
 from flask_restful import Api, Resource
 from datetime import datetime
 from werkzeug.utils import secure_filename
+from os import environ
+RUN_HOST = environ.get('RUN_HOST')
 
-import os
 app = Flask(__name__)
 api = Api(app)
 app.config['DEBUG'] = True
 
 f = open('./data/data.json', 'r')
 images = json.load(f)
-print(images['1'])
 
 
 def updateJSON(images):
@@ -29,19 +30,29 @@ class Images(Resource):
         if(args.get('id') != None):
             try:
                 id = int(args['id'])
-                if int(args['id']) > len(images):
+                if id > len(images):
                     return {"error": "Not Found"}, 404
                 return {"image": images[args['id']]}
             except:
                 return {"error": "Invalid Id"}, 404
         return {"images": images}
 
-    def post(self):
+    def put(self):
         likes = request.form.get('likes')
         author = request.form.get('author')
         title = request.form.get('title')
         image = request.files.get('image')
+        print(likes, author, title)
+        if(author == None):
+            return {"error": "Enter valid author"}, 500
+        if(title == None):
+            return {"error": "Enter valid title"}, 500
+        if(likes == None):
+            return {"error": "Enter valid likes"}, 500
+        if(image == None):
+            return {"error": "Enter valid image"}, 500
         path = f'{str(datetime.timestamp(datetime.now())).split(".")[0]}_{image.filename}'
+
         image.save(os.path.join('data/images/',
                                 secure_filename(path)))
         images[str(len(images)+1)] = {
@@ -51,7 +62,7 @@ class Images(Resource):
             "title": title
         }
         updateJSON(images)
-        return {"images": images}
+        return {"images": images}, 201
 
 
 class File(Resource):
@@ -61,11 +72,15 @@ class File(Resource):
 
 class Like(Resource):
     def get(self, id):
-        if(int(id) > len(images)):
-            return {"error": "Not Found"}, 404
-        images[id]["likes"] += 1
-        updateJSON(images)
-        return {"image": images[id]}
+        try:
+            id_int = int(id)
+            if(id_int > len(images)):
+                return {"error": "Not Found"}, 404
+            images[id]["likes"] += 1
+            updateJSON(images)
+            return {"image": images[id]}, 201
+        except:
+            return {"error": "Invalid Id"}, 404
 
 
 api.add_resource(HelloWorld, "/")
